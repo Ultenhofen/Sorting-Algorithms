f6561038bd793a2662e39d23af747faa6436b848
Maximilian Altenhofen <48171863+Ultenhofen@users.noreply.github.com>
2020-12-01 11:25:43 -0800

    Update insertSort.py

--- insertSort.py
+++ insertSort.py
@@ -1,6 +1,3 @@
-from datetime import datetime
-from timeit import default_timer as timer
-
 def insertionSort(list):
     for i in range(1, len(list)):           # Starting with the second value,
         x = list[i]                       # Compare the selected value with
@@ -9,27 +6,3 @@ def insertionSort(list):
                 list[j + 1] = list[j]       # the first index that it is less than
                 j -= 1                      # To sort Greatest to Least, flip the '<'
         list[j + 1] = x
-
-# Main
-# Pull data from data.txt
-file = open("data.txt" , "r")
-ins = file.readline()
-file.close()
-ins = ins.split(" ")
-ins_n = []
-for i in range(len(ins)):
-    ins_n.append(int(ins[i]))
-
-# Call Insetion Sort
-start = timer()
-insertionSort(ins_n)
-e_t = timer() - start
-print(e_t)
-# Clear insert.out
-open("insert.out" , "w").close()
-file = open("insert.out" , "a+")
-
-# Fill insert.out with the sorted values
-for i in range(len(ins_n)):
-    file.write(str(ins_n[i]) + " ")
-file.close()
